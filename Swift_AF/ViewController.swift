68ef0daf40f2d60e4bd1ce19dcafb3409fea20df
lee <lee@leedemacbook-pro.local>
2016-05-12 14:32:09 +0800

    Initial Commit

---
+++ Swift_AF/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Swift_AF
+//
+//  Created by lee on 16/5/12.
+//  Copyright © 2016年 lee. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
